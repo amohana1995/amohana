0d28f782c455ca3945694bf996eef2b7ed433e97
amohana1995 <56594604+amohana1995@users.noreply.github.com>
2019-10-15 17:08:05 +0300

    Add files via upload

---
+++ index.android.js
@@ -0,0 +1,13 @@
+
+/*
+* This is the start point of the app on the Android platform
+* You can ignore this file since it only links to the src/Main.js file*/
+import { AppRegistry } from 'react-native';
+import Main from './src/Main';
+import App from './src/App';
+import {I18nManager} from 'react-native';
+
+I18nManager.allowRTL(false)
+
+
+AppRegistry.registerComponent('Franchise2', () => App);
